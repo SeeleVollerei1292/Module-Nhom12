83e29db5f82cad2d933c2bb2a0b7f593f407c850
SeeleVollerei1292 <nguyentungminer099@gmail.com>
2025-06-02 13:52:30 +0700

    comit

---
+++ Data/MyDbContext/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.MyDbContext
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entities here
+        }
+    }
+}
